f10a27cc2c58e233d24825be4a6ac594d213a5c4
pritesh <prb407@gmail.com>
2019-05-11 09:23:15 +0530

    multer

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const auth = require("./middleware/auth");
+const multer = require("multer");
+// var storage = multer.diskStorage({
+//   destination: function(req, file, cb) {
+//     cb(null, "/my-uploads");
+//   },
+//   filename: function(req, file, cb) {
+//     console.log("asdasdsada", req.file);
+//     cb(null, file.fieldname + "-" + Date.now());
+//   }
+// });
+
+var upload = multer({ dest: "uploads/" }).single("file");
 
 const app = express();
 
@@ -12,6 +24,16 @@ app.use(auth);
 app.use("/register", require("./routes/register"));
 app.use("/login", require("./routes/login"));
 app.use("/event", require("./routes/event"));
+
+app.post("/multer", async (req, res) => {
+  upload(req, res, function(err) {
+    if (req.file) {
+      console.log(req.file);
+    }
+    console.log(req.file.originalname);
+    // Everything went fine.
+  });
+});
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/mongodbdemo", {
